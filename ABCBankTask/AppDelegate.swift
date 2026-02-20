dcb149f93b385badf0da6bc7125be160b706147f
Elgun Gafarzada <e.gafarzada@andersenlab.com>
2026-02-20 20:06:15 +0400

    Fix duplicate Info.plist build error

--- ABCBankTask/AppDelegate.swift
+++ ABCBankTask/AppDelegate.swift
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let cfg = UISceneConfiguration(name: "Default", sessionRole: connectingSceneSession.role)
+        let cfg = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
         cfg.delegateClass = SceneDelegate.self
         return cfg
     }
