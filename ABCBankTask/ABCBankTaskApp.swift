ae38a65a6c9e0c9453ccafcafad48b55aae01720
Elgun Gafarzada <e.gafarzada@andersenlab.com>
2026-02-20 15:40:40 +0400

    Initial Commit

---
+++ ABCBankTask/ABCBankTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABCBankTaskApp.swift
+//  ABCBankTask
+//
+//  Created by Elgun Gafarzada on 20.02.26.
+//
+
+import SwiftUI
+
+@main
+struct ABCBankTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
