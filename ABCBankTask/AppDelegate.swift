5548340edef8b3cc67d4c9e8671e115b2ee07760
Elgun Gafarzada <e.gafarzada@andersenlab.com>
2026-02-20 19:07:35 +0400

    uikit app setup, remove swiftui entry

---
+++ ABCBankTask/AppDelegate.swift
@@ -0,0 +1,17 @@
+//  AppDelegate.swift
+//  Created by Elgun Gafarzada on 20.02.26.
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let cfg = UISceneConfiguration(name: "Default", sessionRole: connectingSceneSession.role)
+        cfg.delegateClass = SceneDelegate.self
+        return cfg
+    }
+}
---
+++ ABCBankTask/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//  SceneDelegate.swift
+//  Created by Elgun Gafarzada on 20.02.26.
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let ws = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: ws)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+    }
+}
